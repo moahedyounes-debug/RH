fcfe04099e5bb6262482b8e455cf20521bf1cfe2
moahedyounes-debug <moahedyounes@gmail.com>
2026-04-22 13:24:40 +0300

    Update API_URL to new endpoint

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
    API URL
 =========================== */
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbxmubWqAMJYxTaVX8Ax_YTKT4qqH2MFQXER9VztXGLhv7gfcVpaNmvMYMU7Q-j4pkDJIQ/exec";
+  "https://script.google.com/macros/s/AKfycbwYRi7IFVTQ80wR2_VLrOrRb5ugvzSePw6rX6R6dDGEy-GWwciQIseeH36kknZXs3TfaA/exec";
 
 /* ===========================
    POST Wrapper
